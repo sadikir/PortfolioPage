ad1c20f05e336d90d5b150daec68be5888dd9a6b
sadiki rungo <76570335+sadikir@users.noreply.github.com>
2021-05-23 17:58:04 -0600

    fixed menu

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let menu = document.querySelector(".menu");
 icons.addEventListener("click",() =>{
 menu.classList.toggle("change");
 })
+let menuLink = document.querySelectorAll(".link");
+for(let i = 0; i < menuLink.length; i++){
+  menuLink[i].addEventListener("click", () => {
+    menu.classList.toggle("change");
+  })
+}
